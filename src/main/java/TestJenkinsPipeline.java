deb01bab51c82ae9eb4ce3dcfbd386a441925f2e
Noor Sadiya <Noor Sadiya@G6SHW5H2E.logon.ds.ge.com>
2018-08-06 15:31:01 +0530

    Second Commit to Test1Branch

--- src/main/java/TestJenkinsPipeline.java
+++ src/main/java/TestJenkinsPipeline.java
@@ -3,5 +3,6 @@ public class TestJenkinsPipeline {
 
 	public static void main(String[] args) {
 		System.out.println("My First Step into Jenkins");
+		System.out.println("Commit 2 to Test1Branch");
 	}
 }
