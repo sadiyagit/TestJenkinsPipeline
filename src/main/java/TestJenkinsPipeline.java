34d0af84ed31febce9e31675f2387e65b7c46a89
sadiyagit <41662523+sadiyagit@users.noreply.github.com>
2018-08-06 15:27:23 +0530

    First Commit to Master - Direct

--- src/main/java/TestJenkinsPipeline.java
+++ src/main/java/TestJenkinsPipeline.java
@@ -3,5 +3,6 @@ public class TestJenkinsPipeline {
 
 	public static void main(String[] args) {
 		System.out.println("My First Step into Jenkins");
+		System.out.println("Commit 1 to Master");
 	}
 }
